7ffd64e755ce6f72f2c2742ec2f4c02736dca27d
rahul-begames <60950973+RahulChandra99@users.noreply.github.com>
2023-02-08 19:05:51 +0530

    first commit

---
+++ RPG Game/RPG Game/main.cpp
@@ -0,0 +1,20 @@
+//extra library includes
+
+#include<iostream> //input and output stream
+#include<string> 
+
+using namespace std;
+
+void main() {
+	//all of our starter code will start here
+
+	//cout = console out,cin = console input
+	cout << "Hello World!" << endl;
+
+	string name;
+	cin >> name;
+
+	cout << "You suck " << name <<endl;
+	//lets user interact by pressing anykey
+	system("pause");
+}
